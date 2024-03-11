9812687ede2247864fbca877a8eca20f7533e0ce
S <129375913+datagonerogue@users.noreply.github.com>
2024-03-11 18:43:05 +0000

    Adding my details to the template

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,15 +16,15 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/dark_clouds.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "purple and pink abstract background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a UI/UX Web Developer. I enjoy creating unique and simplistic user interfaces in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -35,7 +35,6 @@ const skillsList = [
   "User experience",
   "Inclusive design",
   "Focus group testing",
-  "Mobile user interfaces",
   "Graphic design",
 ];
 
